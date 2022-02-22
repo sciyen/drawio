6e7e7736e3bb8b81835d501a5c875bb0d47872a2
sciyen <a885566885566@gmail.com>
2022-02-22 20:12:34 +0800

    Update

--- src/main/webapp/plugins/zotero.js
+++ src/main/webapp/plugins/zotero.js
@@ -8,142 +8,141 @@ const str_token = '::';
 
 /* get author information */
 function get_author(authors) {
-    if (typeof authors === 'undefined' || authors.length <= 0)
-        return "Unknown author"
-    if (authors.length >= 3)
-        return authors[0].lastName + " et.al."
-    if (authors.length == 2)
-        return authors[0].lastName + " & " + authors[1].lastName
-    return authors[0].lastName;
+	if (typeof authors === 'undefined' || authors.length <= 0)
+		return "Unknown author"
+	if (authors.length >= 3)
+		return authors[0].lastName + " et.al."
+	if (authors.length == 2)
+		return authors[0].lastName + " & " + authors[1].lastName
+	return authors[0].lastName;
 }
 
 /* get year information */
 function get_year(date) {
-    function filter_year(tokens) {
-        for (const t of tokens) {
-            y = parseInt(t)
-            if (!isNaN(y) && y > 50 && y < 10000)
-                return t
-        }
-    }
-    if (typeof date === 'undefined' || date == "")
-        return "Unknown year"
-    return filter_year(date.split(/[-/,]/))
+	function filter_year(tokens) {
+		for (const t of tokens) {
+			y = parseInt(t)
+			if (!isNaN(y) && y > 50 && y < 10000)
+				return t
+		}
+	}
+	if (typeof date === 'undefined' || date == "")
+		return "Unknown year"
+	return filter_year(date.split(/[-/,]/))
 }
 
 /* get details*/
 function get_details(collection_name, title) {
-    return collection_name + str_token + title
+	return collection_name + str_token + title
 }
 
 /* Retrieve library from Zotero API */
 async function retreive(ApiKey, Uid, callback) {
-    counter = 1
-    drawio_tags = [];
-    const myapi = zoteroApi(ApiKey, {
-        'limit': 100
-    }).library('user', Uid)
+	counter = 1
+	drawio_tags = [];
+	const myapi = zoteroApi(ApiKey, {
+		'limit': 100
+	}).library('user', Uid)
 
-    try{
-        const collectionsRes = await myapi.collections().get();
+	try {
+		const collectionsRes = await myapi.collections().get();
 
-        collection_names = {}
+		collection_names = {}
 
-        console.log(collectionsRes)
-        promises = []
+		console.log(collectionsRes)
+		promises = []
 
-        // Retrieve collection information
-        for (const [i, c] of collectionsRes.raw.entries()) {
-            console.log(c)
-            collection_names[c.key] = c.data.name
+		// Retrieve collection information
+		for (const [i, c] of collectionsRes.raw.entries()) {
+			console.log(c)
+			collection_names[c.key] = c.data.name
 
-            // Add promises to request for the items in collection
-            promises.push(new Promise((resolve, reject) => {
-                const itemRes = myapi.collections(c.key).items().get()
-                resolve(itemRes)
-            }))
-            console.log(c.data.name)
-        }
+			// Add promises to request for the items in collection
+			promises.push(new Promise((resolve, reject) => {
+				const itemRes = myapi.collections(c.key).items().get()
+				resolve(itemRes)
+			}))
+			console.log(c.data.name)
+		}
 
-        // Append the content when the data available
-        for (const p of promises) {
-            p.then((itemRes) => {
-                const items = itemRes.getData()
-                items.forEach(item => {
-                    if (item.itemType != "attachment") {
-                        console.log(item)
-                        number = (typeof item.callNumber === 'undefined') ? ('') : (String(item.callNumber))
-                        
-                        item.collections.forEach((ckey) => {
-                            collection_name = collection_names[ckey]
+		// Append the content when the data available
+		for (const p of promises) {
+			p.then((itemRes) => {
+				const items = itemRes.getData()
+				items.forEach(item => {
+					if (item.itemType != "attachment") {
+						console.log(item)
+						number = (typeof item.callNumber === 'undefined') ? ('') : (String(item.callNumber))
 
-                            // Generate metadata for drawio plugin
-                            kname = '[' + number + str_token
-                                + get_details(collection_name, item.title) + str_token
-                                + get_author(item.creators) + " "
-                                + get_year(item.date) + str_token
-                                + item.key + ']'
-                            // \u4e00-\u9fa5 is used to match Chinese character
-                            kname = kname.replace(/[^a-zA-Z0-9/.,&:\]\[\u4e00-\u9fa5]/g, "_")
-                            drawio_tags.push(kname)
-                        })
-                        counter += 1
-                    }
-                })
-            })
-        }
+						item.collections.forEach((ckey) => {
+							collection_name = collection_names[ckey]
 
-        // Wait for all promise to finish no matter if it succeeded or rejected
-        Promise.allSettled(promises).then((result) => {
-            callback(drawio_tags)
-        })   
-    }
-    catch (err){
-        console.log(err)
-        alert("Error: " + String(err) + '\nPlease check the UID and API key!')
-    }
+							// Generate metadata for drawio plugin
+							kname = '[' + number + str_token
+								+ get_details(collection_name, item.title) + str_token
+								+ get_author(item.creators) + " "
+								+ get_year(item.date) + str_token
+								+ item.key + ']'
+							// \u4e00-\u9fa5 is used to match Chinese character
+							kname = kname.replace(/[^a-zA-Z0-9/.,&:\]\[\u4e00-\u9fa5]/g, "_")
+							drawio_tags.push(kname)
+						})
+						counter += 1
+					}
+				})
+			})
+		}
+
+		// Wait for all promise to finish no matter if it succeeded or rejected
+		Promise.allSettled(promises).then((result) => {
+			callback(drawio_tags)
+		})
+	}
+	catch (err) {
+		console.log(err)
+		alert("Error: " + String(err) + '\nPlease check the UID and API key!')
+	}
 }
 
 function loadZoteroTags(ui) {
-    // disable button
-    let action = ui.actions.get('reloadZotero');
-    action.enabled = false;
+	// disable button
+	let action = ui.actions.get('reloadZotero');
+	action.enabled = false;
 
-    // load from Zotero Api and add them to the list of tags (tags for the root)
-    config = JSON.parse(localStorage.getItem(".configuration"));
-    zotero_uid = parseInt(config['zotero_uid'], 10);
-    zotero_api_key = config['zotero_api_key'];
+	// load from Zotero Api and add them to the list of tags (tags for the root)
+	config = JSON.parse(localStorage.getItem(".configuration"));
+	zotero_uid = parseInt(config['zotero_uid'], 10);
+	zotero_api_key = config['zotero_api_key'];
 
-    graph = ui.editor.graph;
-    root = graph.model.getRoot();
+	graph = ui.editor.graph;
+	root = graph.model.getRoot();
 
-    retreive(zotero_api_key, zotero_uid, (citations) => {
-        graph.addTagsForCells([root], citations)
-        action.enabled = true;
-    });
+	retreive(zotero_api_key, zotero_uid, (citations) => {
+		graph.addTagsForCells([root], citations)
+		action.enabled = true;
+	});
 }
 
 function setupZoretoMenu(ui) {
-    // Adds resource for action
-    mxResources.parse('reloadZotero=Reload Zotero...');
+	// Adds resource for action
+	mxResources.parse('reloadZotero=Reload Zotero...');
 
-    // Adds action
-    ui.actions.addAction('reloadZotero', () => {
-        loadZoteroTags(ui);
-    });
+	// Adds action
+	ui.actions.addAction('reloadZotero', () => {
+		loadZoteroTags(ui);
+	});
 
-    // Adds menu item for refreshing
-    let menu = ui.menus.get('extras');
+	// Adds menu item for refreshing
+	let menu = ui.menus.get('extras');
 	let oldFunct = menu.funct;
-	
-	menu.funct = function(menu, parent)
-	{   
-        oldFunct.apply(this, arguments);
-        ui.menus.addMenuItems(menu, ['-', 'reloadZotero'], parent, );
+
+	menu.funct = function (menu, parent) {
+		oldFunct.apply(this, arguments);
+		ui.menus.addMenuItems(menu, ['-', 'reloadZotero'], parent,);
 	};
 }
 
-var TagSelectorWindow = function(editorUi, x, y, w, h){
+var TagSelectorWindow = function (editorUi, x, y, w, h) {
 	var graph = editorUi.editor.graph;
 
 	var div = document.createElement('div');
@@ -171,226 +170,222 @@ var TagSelectorWindow = function(editorUi, x, y, w, h){
 	this.window.setClosable(true);
 	this.window.contentWrapper.style.overflowY = 'scroll';
 
-	mxEvent.addListener(filterInput, 'keyup', function(){
+	mxEvent.addListener(filterInput, 'keyup', function () {
 		// Do something
 		console.log('keyup')
 	});
 
-	this.window.addListener('show', mxUtils.bind(this, function(){
+	this.window.addListener('show', mxUtils.bind(this, function () {
 		this.window.fit();
-		
-		if (this.window.isVisible()){
+
+		if (this.window.isVisible()) {
 		}
-		else{
+		else {
 			graph.container.focus();
 		}
 	}));
-	
-	this.window.setLocation = function(x, y){
+
+	this.window.setLocation = function (x, y) {
 		var iw = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
 		var ih = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
-		
+
 		x = Math.max(0, Math.min(x, iw - this.table.clientWidth));
 		y = Math.max(0, Math.min(y, ih - this.table.clientHeight - 48));
 
-		if (this.getX() != x || this.getY() != y){
+		if (this.getX() != x || this.getY() != y) {
 			mxWindow.prototype.setLocation.apply(this, arguments);
 		}
 	};
-	
-	var resizeListener = mxUtils.bind(this, function(){
+
+	var resizeListener = mxUtils.bind(this, function () {
 		var x = this.window.getX();
 		var y = this.window.getY();
-		
+
 		this.window.setLocation(x, y);
 	});
-	
+
 	mxEvent.addListener(window, 'resize', resizeListener);
 
-	this.destroy = function(){
+	this.destroy = function () {
 		mxEvent.removeListener(window, 'resize', resizeListener);
 		this.window.destroy();
 	}
 }
 
-function setupTagSelector(ui){
+function setupTagSelector(ui) {
 	// Adds resource for action
-    mxResources.parse('tagSelector=Tag Selector');
+	mxResources.parse('tagSelector=Tag Selector');
 
-    // Adds action
-    ui.actions.addAction('tagSelector...', () => {
-        if (editorUi.tagSelectorWindow == null)
-		{
+	// Adds action
+	ui.actions.addAction('tagSelector...', () => {
+		if (editorUi.tagSelectorWindow == null) {
 			editorUi.tagSelectorWindow = new TagSelectorWindow(editorUi, document.body.offsetWidth - 380, 120, 300, 240);
-			editorUi.tagSelectorWindow.window.addListener('show', function()
-			{
+			editorUi.tagSelectorWindow.window.addListener('show', function () {
 				editorUi.fireEvent(new mxEventObject('tagSelector'));
 			});
-			editorUi.tagSelectorWindow.window.addListener('hide', function()
-			{
+			editorUi.tagSelectorWindow.window.addListener('hide', function () {
 				editorUi.fireEvent(new mxEventObject('tagSelector'));
 			});
 			editorUi.tagSelectorWindow.window.setVisible(true);
 			editorUi.fireEvent(new mxEventObject('tagSelector'));
 		}
-		else
-		{
+		else {
 			editorUi.tagSelectorWindow.window.setVisible(!editorUi.tagSelectorWindow.window.isVisible());
 		}
-    });
+	});
 
-    // Adds menu item for refreshing
-    let menu = ui.menus.get('extras');
+	// Adds menu item for refreshing
+	let menu = ui.menus.get('extras');
 	let oldFunct = menu.funct;
-	
-	menu.funct = function(menu, parent)
-	{   
-        oldFunct.apply(this, arguments);
-        ui.menus.addMenuItems(menu, ['-', 'Tag Selector'], parent);
+
+	menu.funct = function (menu, parent) {
+		oldFunct.apply(this, arguments);
+		ui.menus.addMenuItems(menu, ['-', 'Tag Selector'], parent);
 	};
 }
 
 script.onload = () => {
-zoteroApi = ZoteroApiClient.default;
-Draw.loadPlugin(function (ui) {
-    setupZoretoMenu(ui);
+	zoteroApi = ZoteroApiClient.default;
+	Draw.loadPlugin(function (ui) {
+		setupZoretoMenu(ui);
+		setupTagSelector(ui);
 
-	// Adds numbered toggle property
-	Editor.commonVertexProperties.push({
-		name: 'numbered', dispName: 'Numbered', type: 'bool', defVal: true, isVisible: function (state, format) {
-			var graph = format.editorUi.editor.graph;
+		// Adds numbered toggle property
+		Editor.commonVertexProperties.push({
+			name: 'numbered', dispName: 'Numbered', type: 'bool', defVal: true, isVisible: function (state, format) {
+				var graph = format.editorUi.editor.graph;
 
-			return graph.view.redrawNumberShape != null;
-		}, onChange: function (graph, newValue) {
-			graph.refresh();
-		}
-	});
+				return graph.view.redrawNumberShape != null;
+			}, onChange: function (graph, newValue) {
+				graph.refresh();
+			}
+		});
 
-	var graph = ui.editor.graph;
-	var enabled = true;
+		var graph = ui.editor.graph;
+		var enabled = true;
 
-	var graphViewResetValidationState = graph.view.resetValidationState;
+		var graphViewResetValidationState = graph.view.resetValidationState;
 
-	graph.view.resetValidationState = function () {
-		graphViewResetValidationState.apply(this, arguments);
-		this.numberCounter = 0;
-	};
+		graph.view.resetValidationState = function () {
+			graphViewResetValidationState.apply(this, arguments);
+			this.numberCounter = 0;
+		};
 
-	var graphViewValidateCellState = graph.view.validateCellState;
+		var graphViewValidateCellState = graph.view.validateCellState;
 
-	graph.view.validateCellState = function (cell, recurse) {
-		var state = graphViewValidateCellState.apply(this, arguments);
-		recurse = (recurse != null) ? recurse : true;
+		graph.view.validateCellState = function (cell, recurse) {
+			var state = graphViewValidateCellState.apply(this, arguments);
+			recurse = (recurse != null) ? recurse : true;
 
-		if (recurse && state != null && graph.model.isVertex(state.cell) &&
-			mxUtils.getValue(state.style, 'numbered', 1) == 1) {
-			this.numberCounter++;
-			this.redrawNumberShape(state);
-		}
+			if (recurse && state != null && graph.model.isVertex(state.cell) &&
+				mxUtils.getValue(state.style, 'numbered', 1) == 1) {
+				this.numberCounter++;
+				this.redrawNumberShape(state);
+			}
 
-		return state;
-	};
+			return state;
+		};
 
-	function citation_pretty_print(token) {
-		return token[0] + ': ' + token[3].replace(/[^a-zA-Z0-9/.,&:\]\[]/g, " ") + ']'
-	}
-
-	function get_citation_info(tag) {
-		token = tag.split('::')
-		item = {
-			'id': token[0],
-			'collection': token[1],
-			'title': token[2],
-			'citation': citation_pretty_print(token),
-			'key': token[4]
-		}
-		return item
-	}
-
-	graph.view.redrawNumberShape = function (state) {
-		var numbered = mxUtils.getValue(state.style, 'numbered', 1) == 1;
-
-		// Generate tag item
-		citation = ''
-		tags = graph.getTagsForCell(state.cell);
-		if (tags.length > 0) {
-			tags.split(' ').forEach((tag) => {
-				info = get_citation_info(tag)
-				citation += info.citation + ' \n'
-			})
+		function citation_pretty_print(token) {
+			return token[0] + ': ' + token[3].replace(/[^a-zA-Z0-9/.,&:\]\[]/g, " ") + ']'
 		}
 
-		var value = '<div style="padding:2px;border:1px solid gray;background:yellow;border-radius:2px;">'
-			+ citation + '</div>';
-
-
-		if (enabled && numbered && (citation != '') && graph.model.isVertex(state.cell) &&
-			state.shape != null && state.secondLabel == null) {
-			state.secondLabel = new mxText(value, new mxRectangle(),
-				mxConstants.ALIGN_LEFT, mxConstants.ALIGN_BOTTOM);
-
-			// Styles the label
-			state.secondLabel.size = 12;
-			state.secondLabel.dialect = mxConstants.DIALECT_STRICTHTML;
-			graph.cellRenderer.initializeLabel(state, state.secondLabel);
+		function get_citation_info(tag) {
+			token = tag.split('::')
+			item = {
+				'id': token[0],
+				'collection': token[1],
+				'title': token[2],
+				'citation': citation_pretty_print(token),
+				'key': token[4]
+			}
+			return item
 		}
 
-		if (state.secondLabel != null) {
-			if (!numbered) {
+		graph.view.redrawNumberShape = function (state) {
+			var numbered = mxUtils.getValue(state.style, 'numbered', 1) == 1;
+
+			// Generate tag item
+			citation = ''
+			tags = graph.getTagsForCell(state.cell);
+			if (tags.length > 0) {
+				tags.split(' ').forEach((tag) => {
+					info = get_citation_info(tag)
+					citation += info.citation + ' \n'
+				})
+			}
+
+			var value = '<div style="padding:2px;border:1px solid gray;background:yellow;border-radius:2px;">'
+				+ citation + '</div>';
+
+
+			if (enabled && numbered && (citation != '') && graph.model.isVertex(state.cell) &&
+				state.shape != null && state.secondLabel == null) {
+				state.secondLabel = new mxText(value, new mxRectangle(),
+					mxConstants.ALIGN_LEFT, mxConstants.ALIGN_BOTTOM);
+
+				// Styles the label
+				state.secondLabel.size = 12;
+				state.secondLabel.dialect = mxConstants.DIALECT_STRICTHTML;
+				graph.cellRenderer.initializeLabel(state, state.secondLabel);
+			}
+
+			if (state.secondLabel != null) {
+				if (!numbered) {
+					state.secondLabel.destroy();
+					state.secondLabel = null;
+				}
+				else {
+					var scale = graph.getView().getScale();
+					var bounds = new mxRectangle(state.x + state.width - 4 * scale, state.y + 4 * scale, 0, 0);
+					state.secondLabel.value = value;
+					state.secondLabel.state = state;
+					state.secondLabel.scale = scale;
+					state.secondLabel.bounds = bounds;
+					state.secondLabel.redraw();
+				}
+			}
+		};
+
+		// Destroys the shape number
+		var destroy = graph.cellRenderer.destroy;
+		graph.cellRenderer.destroy = function (state) {
+			destroy.apply(this, arguments);
+
+			if (state.secondLabel != null) {
 				state.secondLabel.destroy();
 				state.secondLabel = null;
 			}
-			else {
-				var scale = graph.getView().getScale();
-				var bounds = new mxRectangle(state.x + state.width - 4 * scale, state.y + 4 * scale, 0, 0);
-				state.secondLabel.value = value;
-				state.secondLabel.state = state;
-				state.secondLabel.scale = scale;
-				state.secondLabel.bounds = bounds;
-				state.secondLabel.redraw();
-			}
+		};
+
+		graph.cellRenderer.getShapesForState = function (state) {
+			return [state.shape, state.text, state.secondLabel, state.control];
+		};
+
+		// Extends View menu
+		mxResources.parse('number=Number');
+
+		// Adds action
+		var action = ui.actions.addAction('number...', function () {
+			enabled = !enabled;
+			graph.refresh();
+		});
+
+		action.setToggleAction(true);
+		action.setSelectedCallback(function () { return enabled; });
+
+		var menu = ui.menus.get((urlParams['sketch'] == '1') ? 'extras' : 'view');
+		var oldFunct = menu.funct;
+
+		menu.funct = function (menu, parent) {
+			oldFunct.apply(this, arguments);
+
+			ui.menus.addMenuItems(menu, ['-', 'number'], parent);
+		};
+
+		// Forces refresh if file was loaded before plugin
+		if (ui.getCurrentFile() != null) {
+			graph.refresh();
 		}
-	};
-
-	// Destroys the shape number
-	var destroy = graph.cellRenderer.destroy;
-	graph.cellRenderer.destroy = function (state) {
-		destroy.apply(this, arguments);
-
-		if (state.secondLabel != null) {
-			state.secondLabel.destroy();
-			state.secondLabel = null;
-		}
-	};
-
-	graph.cellRenderer.getShapesForState = function (state) {
-		return [state.shape, state.text, state.secondLabel, state.control];
-	};
-
-	// Extends View menu
-	mxResources.parse('number=Number');
-
-	// Adds action
-	var action = ui.actions.addAction('number...', function () {
-		enabled = !enabled;
-		graph.refresh();
 	});
-
-	action.setToggleAction(true);
-	action.setSelectedCallback(function () { return enabled; });
-
-	var menu = ui.menus.get((urlParams['sketch'] == '1') ? 'extras' : 'view');
-	var oldFunct = menu.funct;
-
-	menu.funct = function (menu, parent) {
-		oldFunct.apply(this, arguments);
-
-		ui.menus.addMenuItems(menu, ['-', 'number'], parent);
-	};
-
-	// Forces refresh if file was loaded before plugin
-	if (ui.getCurrentFile() != null) {
-		graph.refresh();
-	}
-});
 };
